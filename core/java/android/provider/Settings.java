ecbf9b7fa58b2e34551fbbde703f38451aa3eca4
chickenfactory <micah.chickenfactory@gmail.com>
2015-04-29 23:54:04 -1000

    Revert "SystemBars: Allow the StatusBarComponent be externally defined"
    This reverts commit 7f0598efa12abf78360d021315123bcb7b0e7337.
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4487,13 +4487,6 @@ public final class Settings {
                 "volume_keys_control_ring_stream";
 
         /**
-         * The statusbar configuration to be used for its creation in SystemUI
-         * Fallback is the config_statusBarComponent value
-         * @hide
-         */
-        public static final String STATUSBAR_COMPONENT = "statusbar_component";
-
-        /**
          * This preference enables showing the power menu on LockScreen.
          * @hide
          */
